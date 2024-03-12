ce1f3c4cdad3862194ef9f113aa2a6468f2e6aa9
lwnlia <162718621+lwnlia@users.noreply.github.com>
2024-03-12 11:23:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src="https://www.japaoemfoco.com/wp-content/uploads/2011/03/o-fen%C3%B4meno-hello-kitty.jpg"
+}
+function mudarimagemdentro()
+ {
+    document.getElementById("imagem").src ="https://ogimg.infoglobo.com.br/cultura/13748681-aea-0a0/FT1086A/Hello.jpg"
+ }
